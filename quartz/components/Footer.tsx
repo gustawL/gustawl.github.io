8a74e2fa56ccc1710e38336de5a4f4e46b0b9e51
gustawL <lizak.gustaw@pm.me>
2025-12-17 15:09:22 +0100

    Change footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+         Gustaw Lizak © {year}, licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a> 
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
